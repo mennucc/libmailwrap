aea77e9185eb9cde09bc825ec6fb3790e28bb6a5
Andrea C. G. Mennucci <andrea.mennucci@gmail.com>
2025-08-18 15:10:18 +0200

    LMW_send_email_direct.c  to show a very simple way to use the library code

---
+++ LMW_send_email_direct.c
@@ -0,0 +1,59 @@
+// vim:ts=4:shiftwidth=4:et
+/*
+   tester program for email sender
+
+   will send email
+
+   This is a very simple example, where the library C code
+   is simply included inline
+   
+   Copyright (c) by Andrea C G Mennucci
+    
+   LICENSE
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License
+   as published by the Free Software Foundation; either version 2
+   of the License, or (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+*/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define LMW_DEBUG
+#include "LMW_send_email.c"
+
+int main(int argc , char *argv[])
+{
+  if(argc<4) {
+    fprintf(stderr,"Usage:  %s RECIPIENT SUBJECT BODY\n"
+	    ,argv[0]);
+    return(0);
+  }
+
+  char *recipient= argv[1],
+    *subject =  argv[2],
+    *body =  argv[3];
+  
+  LMW_config *cfg = malloc(sizeof(LMW_config));
+  LWM_config_init(cfg);
+
+  int r;
+  r =LMW_send_email(recipient, subject, body , cfg) ;
+  fprintf(stdout,"return code  %d  ,failures %d \n\n", r, cfg->failures);
+
+  free(cfg);
+  
+  return r;
+}
--- Makefile
+++ Makefile
@@ -1,9 +1,12 @@
 CFLAGS += -Wall
 
-all: LMW_send_email_test LMW_send_email_strestest
+all: LMW_send_email_test LMW_send_email_strestest LMW_send_email_direct
 
 LMW_send_email_test: LMW_send_email_test.c LMW_send_email.c  LMW_send_email.h
 	$(CC) $(CFLAGS)  LMW_send_email_test.c LMW_send_email.c -o LMW_send_email_test
 
 LMW_send_email_strestest: LMW_send_email_stresstest.c LMW_send_email.c  LMW_send_email.h
 	$(CC) $(CFLAGS)  LMW_send_email_stresstest.c LMW_send_email.c -o LMW_send_email_stresstest
+
+LMW_send_email_direct: LMW_send_email_test.c LMW_send_email.c  LMW_send_email.h
+	$(CC) $(CFLAGS)  LMW_send_email_direct.c -o LMW_send_email_direct
